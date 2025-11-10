321eee3550dfa27ed041eb46ae5006f29b0dd8e1
Pandore <lipefmenezes@gmail.com>
2025-11-10 01:50:44 -0300

    Adicionando verificações e limpando código

--- index.js
+++ index.js
@@ -16,27 +16,30 @@ app.get('/', (req, res) => {
 app.get('/jogo-aleatorio', async (req, res) => {
     const { steamid } = req.query;
 
-    if (!steamid) return res.status(400).json({ "erro": "SteamID64 não informada."});
+    if (!steamid || !/^\d{17}$/.test(steamid)) return res.status(400).json({ erro: "SteamID64 não inválida ou ausente."});
 
     try {
         const response = await fetch(`https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${API_KEY}&steamid=${steamid}&format=json&include_appinfo=true`);
 
         if (!response.ok) {
-            return res.status(400).json({ "erro": "Parametros inválidos."});
+            return res.status(400).json({ erro: "Parametros inválidos."});
         }
 
         const data = await response.json();
+
+        if (!data.response?.games?.length) return res.status(404).json({ erro: "Nenhum jogo encontrado na biblioteca."});
+
         const indiceAleatorio = Math.floor(Math.random() * data.response.game_count);
         const jogoEscolhido = data.response.games[indiceAleatorio];
-        return res.status(200).json({ "response": {
+        return res.json({
             "appid": jogoEscolhido.appid,
             "name": jogoEscolhido.name,
             "playtime": jogoEscolhido.playtime_forever,
-        }});
+        });
         
     } catch (error) {
-        console.error("Erro de fetch: ", error);
-        return res.status(500).json({ "erro": "Erro de fetch do servidor."});
+        console.error("Erro da API: ", error.message);
+        return res.status(500).json({ erro: "Erro interno do servidor."});
     }
 });
 
